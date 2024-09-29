413c3f2dc313b0eeb1da5ecf9a472f2a95c12a35
Grade <a.borychevskyi@gmail.com>
2024-09-29 18:01:03 +0300

    fix

--- src/telethon_app/channels_utils.py
+++ src/telethon_app/channels_utils.py
@@ -3,7 +3,7 @@ from telethon.tl.functions.channels import JoinChannelRequest
 from telethon_auto_subsc.logger import get_logger
 from django.conf import settings
 
-import time
+import asyncio
 
 from .telethon_core import TelethonCore
 
@@ -20,19 +20,17 @@ class ChannelsUtils:
     async def subscribe_channels(self, delay: int = 5):
         async with TelegramClient(self.session, self.account.api_id, self.account.api_hash) as client:
             channel_count = len(self.channels)
-            subscribed_channels = 0
             for index, channel in enumerate(self.channels):
                 try:
                     await client(JoinChannelRequest(channel))
-                    if index + 1 < channel_count:
-                        time.sleep(delay)
-
-                    subscribed_channels += 1
+                    logger.info(f'Subscribed to channel {channel}')
                 except Exception as e:
                     logger.error(f'Error while subscribing to channel {channel}: {e}')
-
-            logger.info(f'Subscribed to {subscribed_channels} channels')
-            return subscribed_channels
+                finally:
+                    channel_count -= 1
+                    if channel_count > 1:
+                        await asyncio.sleep(delay)
+            return True
 
     async def send_message(self, message: str, delay: int = 5):
         async with TelegramClient(self.session, self.account.api_id, self.account.api_hash) as client:
@@ -41,10 +39,11 @@ class ChannelsUtils:
                 try:
                     await client.send_message(channel, message)
                     logger.info(f'Message sent to {channel}')
-                    channels_count -= 1
-                    if channels_count > 1:
-                        time.sleep(delay)
+
                 except Exception as e:
                     logger.error(f'Error while sending message to {channel}: {e}')
-
+                finally:
+                    channels_count -= 1
+                    if channels_count > 1:
+                        await asyncio.sleep(delay)
             return True
